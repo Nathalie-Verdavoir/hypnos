86476167a18bbf493edb3fe61fc99fffda1a6b15
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-28 15:28:47 +0200

    Add button on photos of hotel to show, remove and set as cover (with svg)

--- src/Repository/PhotoRepository.php
+++ src/Repository/PhotoRepository.php
@@ -45,22 +45,21 @@ class PhotoRepository extends ServiceEntityRepository
         }
     }
 
-    // /**
-    //  * @return Photo[] Returns an array of Photo objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+      * @return Photo[] Returns an array of Photo objects
+      */
+    
+    public function findByHotel($value)
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
+            ->andWhere('p.hotel = :val')
             ->setParameter('val', $value)
             ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?Photo
