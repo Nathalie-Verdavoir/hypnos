71b0a52ba4bb3aaba7a7ac4ea2ea4643a838a71d
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-04-08 12:17:56 +0200

    CHange settings on date picker

--- public/js/resa.js
+++ public/js/resa.js
@@ -202,12 +202,114 @@ const addEventOnChangeOnChambre = () => $(function() {
 $(document).ready(function(){
     $(function() {
     $('input[name="daterange"]').daterangepicker({
-    "startDate": "01/01/2020",
-    "endDate": "17/01/2020",
-    opens: 'center',
-    locale: {
-    format: 'DD/MM/YYYY'
-    }
+        isInvalidDate: function(arg){
+            // Prepare the date comparision
+            var thisMonth = arg._d.getMonth()+1;   // Months are 0 based
+            if (thisMonth<10){
+                thisMonth = "0"+thisMonth; // Leading 0
+            }
+            var thisDate = arg._d.getDate();
+            if (thisDate<10){
+                thisDate = "0"+thisDate; // Leading 0
+            }
+            var thisYear = arg._d.getYear()+1900;   // Years are 1900 based
+   
+            var thisCompare = thisMonth +"/"+ thisDate +"/"+ thisYear;
+   
+            if($.inArray(thisCompare,disabledArr)!=-1){
+                return true;
+            }
+        },
+      alwaysShowCalendars: true,
+      timePicker: true,
+      startDate,
+      endDate,
+      minDate,
+      locale: {
+        format: 'DD/MM/YYYY',
+        applyLabel: "Ok",
+        cancelLabel: "Annuler",
+        fromLabel: "Du",
+        toLabel: "Au",
+        customRangeLabel: "Custom",
+        separator: ' au ',
+        monthNames: [
+            'Janvier',
+            'Février',
+            'Mars',
+            'Avril',
+            'Mai',
+            'Juin',
+            'Juillet',
+            'Août',
+            'Septembre',
+            'Octobre',
+            'Novembre',
+            'Décembre'
+        ],
+        daysOfWeek: [
+            "D",
+            "L",
+            "M",
+            "M",
+            "J",
+            "V",
+            "S"
+        ],
+        firstDay: 1
+      }
+    }).on("apply.daterangepicker",function(e,picker){
+
+        // Get the selected bound dates.
+        startDate = picker.startDate.format('DD/MM/YYYY')
+        endDate = picker.endDate.format('DD/MM/YYYY')
+    
+        // Compare the dates again.
+        var clearInput = false;
+        for(i=0;i<disabledArr.length;i++){
+            if(startDate<disabledArr[i] && endDate>disabledArr[i]){
+                clearInput = true;
+            }
+        }
+    
+        // If a disabled date is in between the bounds, clear the range.
+        if(clearInput){
+    
+            // To clear selected range (on the calendar).
+
+            const today = new Date()
+            const tomorrow = new Date(today)
+            tomorrow.setDate(tomorrow.getDate() + 1)
+            $(this).data('daterangepicker').setStartDate(today);
+            $(this).data('daterangepicker').setEndDate(tomorrow);
+    
+            // To clear input field and keep calendar opened.
+            $(this).val("").focus();
+    
+            // Alert user!
+            alert("Your range selection includes disabled dates!");
+        }else{
+            $(`#reservation_debut_month option[value='${picker.startDate.format('M')}']`).prop('selected', true);
+            $(`#reservation_debut_day option[value='${picker.startDate.format('D')}']`).prop('selected', true);
+            $(`#reservation_debut_year option[value='${picker.startDate.format('YYYY')}']`).prop('selected', true);
+            $(`#reservation_fin_month option[value='${picker.endDate.format('M')}']`).prop('selected', true);
+            $(`#reservation_fin_day option[value='${picker.endDate.format('D')}']`).prop('selected', true);
+            $(`#reservation_fin_year option[value='${picker.endDate.format('YYYY')}']`).prop('selected', true);
+            start = new Date(picker.startDate);
+            end = new Date(picker.endDate); 
+            let dayCount = 0
+while (end > start) {
+dayCount++
+start.setDate(start.getDate() + 1)
+}
+            $(`#nuitee`).text(dayCount + ' ' + (dayCount>1 ? 'nuitées' : 'nuitée'));
+            chambresPrice.forEach(chambre => {
+                if(chambre.id==$('#chambreSelector').val()){
+                    price=chambre.prix
+                }
+            });
+            $(`#montant`).text(dayCount*price + ' €');
+        }
     });
     });
     });
