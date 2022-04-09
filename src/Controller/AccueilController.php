0d14d410492ca547a179bdd71669149e98f76ff6
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-04-09 19:37:50 +0200

    Change on Accueil and slides with animated text

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\HotelRepository;
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,11 @@ class AccueilController extends AbstractController
 {
 
     #[Route('/', name: 'app_accueil', methods: ['GET'])]
-    public function slide(HotelRepository $hotelRepository): Response
+    public function slide(HotelRepository $hotelRepository, PhotoRepository $photoRepository): Response
     {
         return $this->render('accueil/index.html.twig', [
             'hotels' => $hotelRepository->findAll(),
+            'photos' => $photoRepository->findAll(),
         ]);
     }
     
