f91ec3986df204109e3e5c080a1fc0ad5bac9f48
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-07-09 23:17:01 +0200

    Revert "add remove file from cloudinary"
    This reverts commit f10d7089a29a2bc2300596dc41214a982641ef4a.

--- src/Service/ImageUploader.php
+++ src/Service/ImageUploader.php
@@ -1,32 +1,18 @@
 <?php
  namespace App\Service;
 
-use Cloudinary\Api\ApiResponse;
-use Cloudinary\Api\Upload\UploadApi;
-use Cloudinary\Configuration\Configuration;
+ use Cloudinary\Api\Upload\UploadApi;
+ use Cloudinary\Configuration\Configuration;
 
  class ImageUploader
  {
-
-    public function __construct()
-    {
-      Configuration::instance([ 
-        "cloud_name" => $_ENV['CLOUDINARY_NAME'], 
-        "api_key" => $_ENV['CLOUDINARY_API_KEY'], 
-        "api_secret" => $_ENV['CLOUDINARY_API_SECRET'], 
-        "secure" => FALSE]);
-    }
-
-    public function upload($file, $options = []): ApiResponse
+  public function upload($file, $options = [])
      {
-         $response=(new UploadApi())->upload(  $file, $options);
-         return $response;
-     }
-
-    public function remove($file, $options = []): ApiResponse
-     {
-         $response=(new UploadApi())->destroy(  $file, $options);
-         return $response;
+       Configuration::instance([ 
+                    "cloud_name" => $_ENV['CLOUDINARY_NAME'], 
+                    "api_key" => $_ENV['CLOUDINARY_API_KEY'], 
+                    "api_secret" => $_ENV['CLOUDINARY_API_SECRET'], 
+                    "secure" => FALSE]);
+        (new UploadApi())->upload(  $file,$options);
      }
  }
- 
