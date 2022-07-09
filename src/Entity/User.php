69a1cd14dc527885fbb20846176263a76920ef18
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-07-09 23:17:01 +0200

    Changing unique entity assert to 'Bad Credentials'

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Bad Credentials')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
